cd5313e991db5f479f621492d80086b1a6e99409
hitrch <vladislawpaliy@gmail.com>
2019-04-05 08:58:58 +0300

    almost proxy created

--- node.js
+++ node.js
@@ -1,15 +1,44 @@
-// содежимое index.js
-const http = require('http')
-const port = 3000
-const requestHandler = (request, response) => {
-    console.log(request.url)
 
-    response.end('Hello Node.js Server!')
-}
-const server = http.createServer(requestHandler)
-server.listen(port, (err) => {
-    if (err) {
-        return console.log('something bad happened', err)
-    }
-    console.log(`server is listening on ${port}`)
-})
+const http = require('http');
+//const WebSocket = require('ws');
+
+const server = http.createServer((clientRequest, clientResponse) => {
+    const request = http.get(clientRequest.url, (proxyResp) =>{
+        clientResponse.writeHead(proxyResp.statusCode, proxyResp.headers);
+       proxyResp.pipe(clientResponse);
+    });
+
+
+
+    /*server.on('upgrade', (req, serverSocket) =>{
+        serverSocket.write( "HTTP/1.1 101 Web Socket Protocol Handshake\r\n"
+            + "Upgrade: WebSocket\r\n"
+            + "Connection: Upgrade\r\n"
+            + "WebSocket-Origin: http://localhost:3400\r\n"
+            + "WebSocket-Location: ws://localhost:3400/\r\n"
+            + "\r\n"
+        );
+
+
+    });
+
+    request.on('upgrade', (res, socket) => {
+        console.log('got upgraded!');
+        socket.end();
+        process.exit(0);
+    });
+
+
+    const wss = new WebSocket.Server({ port: 80 });
+
+    wss.on('connection', function connection(ws) {
+        ws.on('message', function incoming(message) {
+            console.log('received: %s', message);
+        });
+
+        ws.send('something');
+    });*/
+
+});
+
+server.listen(8080);
