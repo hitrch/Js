e72e9d87f068919c9425a1bcebd9bb022716aadf
hitrch <vladislawpaliy@gmail.com>
2019-06-15 11:01:18 +0300

    option added

--- parser.js
+++ parser.js
@@ -1,10 +1,14 @@
-'use static';
-
 const Telegraf  = require('telegraf'),
     {rozclad} = require('./function');
 
+const options = {
+    webHook: {
+        port: process.env.PORT
+    }
+};
 
-const bot = new Telegraf(process.env.SECRET_BOT_KEY);
+const bot = new Telegraf(process.env.SECRET_BOT_KEY, options);
+
 let url = 'http://rozklad.kpi.ua/Schedules/ViewSchedule.aspx?g=894be0b0-9c4b-492e-a3d0-a6950cb1a3e1';
 
 bot.start((ctx) => ctx.reply('Welcome'));
