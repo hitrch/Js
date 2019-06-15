0d62e2e8d96412f6f53081155bdbee62d7440891
hitrch <vladislawpaliy@gmail.com>
2019-06-15 11:06:51 +0300

    regex changed

--- parser.js
+++ parser.js
@@ -1,21 +1,14 @@
 const Telegraf  = require('telegraf'),
     {rozclad} = require('./function');
 
-const options = {
-    webHook: {
-        port: process.env.PORT
-    }
-};
-
-const bot = new Telegraf(process.env.SECRET_BOT_KEY, options);
-
+const bot = new Telegraf(process.env.SECRET_BOT_KEY);
 let url = 'http://rozklad.kpi.ua/Schedules/ViewSchedule.aspx?g=894be0b0-9c4b-492e-a3d0-a6950cb1a3e1';
 
 bot.start((ctx) => ctx.reply('Welcome'));
-bot.hears( '',(ctx) => rozclad(url)
+bot.hears( '[[:word:]]',(ctx) => rozclad(url)
     .then(result=>{ctx.reply(result)})
     .catch(()=>ctx.reply('Something went wrong')));
 
-//bot.telegram.setWebhook('https://js.hitrch.now.sh');
+bot.telegram.setWebhook('https://js.hitrch.now.sh');
 
 module.exports = bot.webhookCallback('/');
