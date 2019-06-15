9be2bedec882bbe75f4192230c9a392b52970d08
hitrch <vladislawpaliy@gmail.com>
2019-06-15 10:49:03 +0300

    test

--- parser.js
+++ parser.js
@@ -7,13 +7,10 @@ const Telegraf  = require('telegraf'),
 const bot = new Telegraf(process.env.SECRET_BOT_KEY);
 let url = 'http://rozklad.kpi.ua/Schedules/ViewSchedule.aspx?g=894be0b0-9c4b-492e-a3d0-a6950cb1a3e1';
 
-
-bot.hears(/^[А-ЯІа-яі]{2}-[1-9а-яі]{2,5}$/, (ctx) => rozclad(url)
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.hears( '',(ctx) => rozclad(url)
     .then(result=>{ctx.reply(result)})
     .catch(()=>ctx.reply('Something went wrong')));
-
-
-
 
 bot.telegram.setWebhook('https://js.hitrch.now.sh');
 
