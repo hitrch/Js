b710e20b60bcca4e01987a8aa5ff3987b6320797
hitrch <vladislawpaliy@gmail.com>
2019-06-17 15:28:30 +0300

    greetings changed

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const Telegraf  = require('telegraf'),
 
 const bot = new Telegraf(process.env.SECRET_BOT_KEY);
 
-bot.start((ctx) => ctx.reply('Welcome'));
+bot.start((ctx) => ctx.reply('Welcome. Enter your group(XX-XX)'));
 bot.hears( /.$/,(ctx) => rozclad(ctx.message.text)
     .then(([res1, res2]) => {
         ctx.reply(res1);
         ctx.reply(res2);
     })
-    .catch(() => ctx.reply('Something went wrong. Check if group exists')));
+    .catch(() => ctx.reply('Something went wrong. Check if group exists(XX-XX)')));
 
 bot.telegram.setWebhook('https://js.hitrch.now.sh');
 
