bae9689a417d71fd073f22bc69b1bec14fe5f7f0
hitrch <vladislawpaliy@gmail.com>
2019-06-20 11:08:14 +0300

    ordered via then

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Telegraf  = require('telegraf'),
     {rozclad} = require('./parse');
 
 const bot = new Telegraf(process.env.SECRET_BOT_KEY);
-
+//const bot = new Telegraf('662101909:AAEEPKpvBZ8e648LQ7fQ40Udi9TqGIcTo58');
 bot.start((ctx) => ctx.reply('Welcome. Enter your group(XX-XX)'));
 bot.hears( /.$/,(ctx) => rozclad(ctx.message.text)
     .then(([res1, res2]) => {
@@ -12,7 +12,7 @@ bot.hears( /.$/,(ctx) => rozclad(ctx.message.text)
 
     })
     .catch(() => ctx.reply('Something went wrong. Check if group exists(XX-XX)')));
-
+//bot.launch();
 bot.telegram.setWebhook('https://js.hitrch.now.sh');
 
 module.exports = bot.webhookCallback('/');
