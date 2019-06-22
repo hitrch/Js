37199d26a68758e98a2a277a9547e929f5e261ee
hitrch <vladislawpaliy@gmail.com>
2019-06-22 15:00:45 +0300

    testaxssaadc

--- index.js
+++ index.js
@@ -18,17 +18,18 @@ const bot = new Telegraf(process.env.SECRET_BOT_KEY);
 bot.start((ctx) => ctx.reply('Welcome. Enter your group(XX-XX)'));
 bot.hears( /.$/,(ctx) => {
     let group = ctx.message.text.toLowerCase();
-    console.log(group);
-    let isGroup = checkGroup(group);
+    let isGroup = checkGroup(encodeURI(group));
     isGroup.then(data => {
         if(data !== undefined)
         {
+            console.log("SUSSCUCUSDXuibCISSCDACAAC NOT IN BASE");
             ctx.reply(data.week1).then(() => {
                 ctx.reply(data.week2);
             });
         }else{
             rozclad(group)
                 .then(([res1, res2]) => {
+                    console.log("ERESSSSSSSSS NOT IN BASE");
                     ctx.reply(res1).then(() => {
                         ctx.reply(res2);
                     });
@@ -37,6 +38,7 @@ bot.hears( /.$/,(ctx) => {
                 .catch(() => ctx.reply('Something went wrong. Check if group exists(XX-XX)'));
         }
     });
+    return;
 
 });
 
@@ -56,7 +58,6 @@ function saveGroup(group, res1, res2){
         'week2' : res2
     });
 }
-
 
 
 //bot.telegram.setWebhook('https://js.hitrch.now.sh');
